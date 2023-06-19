51ec3b4c46b98d0843f2ffae5a9459900bdbfc59
AM111D <Dimasssnike@icloud.com>
2023-06-18 21:40:56 -0400

    change

--- contacts.js
+++ contacts.js
@@ -14,45 +14,32 @@ async function listContacts() {
 }
 
 async function getContactById(contactId) {
-  // ...твой код. Возвращает объект контакта с таким id. Возвращает null, если объект с таким id не найден.
-
-  // const data = await fs.readFile(contactsPath);
   const contacts = await listContacts();
-  // const contacts = JSON.parse(data);
-  return (
-    (result = contacts.find((contact) => contact.id === contactId)) || null
-  );
+  const result = contacts.find((contact) => contact.id === contactId);
+  return result || null;
 }
 
 async function removeContact(contactId) {
-  // ...твой код. Возвращает объект удаленного контакта. Возвращает null, если объект с таким id не найден.
   const contacts = await listContacts();
   const index = contacts.findIndex((contact) => contact.id === contactId);
   if (index === -1) return null;
   const [result] = contacts.splice(index, 1);
   await upDateContacts(contacts);
   return result;
-  // return newDeletedContacts;
 }
 
 async function addContact({ name, email, phone }) {
-  try {
-    const contacts = await listContacts();
-    const newContact = {
-      id: nanoid(),
-      name,
-      email,
-      phone,
-    };
-    contacts.push(newContact);
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
 
-    await upDateContacts(contacts);
-
-    return newContact;
-  } catch (error) {
-    console.error("Error adding contact:", error);
-    return null;
-  }
+  await upDateContacts(contacts);
+  return newContact;
 }
 
 module.exports = {
